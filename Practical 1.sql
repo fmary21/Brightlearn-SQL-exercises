45ddac7dbaa25136e8aee8aee1164d88bca9b39d
Fadzai Makanda <fadzaimakanda21@gmail.com>
2025-11-20 18:30:22 +0200

    Update Practical 1.sql

--- Practical 1.sql
+++ Practical 1.sql
@@ -3,26 +3,30 @@
 
 SELECT*
 FROM retail_sales_dataset;
+--------------------------------------------------------------------------------------
 
---Display only the Transaction ID, DATE,and Customer ID for all records?
---Expected output: Transactions ID, Date,Customer ID.
+----Display only the Transaction ID, DATE,and Customer ID for all records?
+----Expected output: Transactions ID, Date,Customer ID.
 
 SELECT transaction_id,
        date,
        customer_id
 FROM retail_sales_dataset;
+-----------------------------------------------------------------------------------------
 
 --Display all the distinct product categories in the dataset?
 --Expected output: Poduct Category.
 
 SELECT DISTINCT product_category
 FROM retail_sales_dataset;
+------------------------------------------------------------------------------------------
 
 --Display all the distinct gender values in the dataset?
 --Expected output:Gender
 
 SELECT DISTINCT gender
 FROM retail_sales_dataset;
+------------------------------------------------------------------------------------------
 
 --Display all transactions where the Age is greater than 40?
 --Expected output: all columns.
@@ -30,6 +34,7 @@ FROM retail_sales_dataset;
 SELECT*
 FROM retail_sales_dataset
 WHERE Age>40;
+------------------------------------------------------------------------------------------------
 
 --Display all transactions where the Price per Unit is between 100 and 500.
 --Expected output: all columns
@@ -37,6 +42,7 @@ WHERE Age>40;
 SELECT *
 FROM retail_sales_dataset
 WHERE price_per_Unit BETWEEN 100 AND 500;
+---------------------------------------------------------------------------------------------
 
 --Display all transactions where the product category is either 'beauty'or 'Electronics'?
 --Expected output: all columns
@@ -44,6 +50,7 @@ WHERE price_per_Unit BETWEEN 100 AND 500;
 SELECT *
 FROM retail_sales_dataset
 WHERE product_category IN ('beauty', 'Electronics');
+------------------------------------------------------------------------------------------------
 
 --Display all transactions where the product category is not 'Clothing'?
 --Expected output: all columns.
@@ -51,6 +58,7 @@ WHERE product_category IN ('beauty', 'Electronics');
 SELECT *
 FROM retail_sales_dataset
 WHERE product_category <> 'Clothing';
+-----------------------------------------------------------------------------------------------
 
 --display all transactions where the Quality is greater or equal to 3?
 --Expected output: all columns
@@ -58,36 +66,42 @@ WHERE product_category <> 'Clothing';
 SELECT*
 FROM retail_sales_dataset
 WHERE Quantity >= 3;
+------------------------------------------------------------------------------------------------
 
 --COUNT the total number of transactions?
 --Expected output: Total_Transactions
 
 SELECT COUNT(*)AS Total_transactions
 FROM retail_sales_dataset,
+----------------------------------------------------------------------------------------------
 
 --Find the average Age of customers.
 --Expected output:Average_Age
 
 SELECT AVG(Age)AS average_age
 FROM retail_sales_dataset,
+---------------------------------------------------------------------------------------------
 
 --Find the total quantity of products sold.
 --Expected output:Total_Quantity
 
 SELECT SUM(quantity)AS quantity_sold
 FROM retail_sales_dataset;
+-----------------------------------------------------------------------------------------------
 
 --Find the maximum Total Amount spent in a single transaction.
 --EXpected output:Max_Total_Amount
 
 SELECT MAX(total_amount)AS Max_Total_Amount
 FROM retail_sales_dataset;
+------------------------------------------------------------------------------------------------
 
 --Find the minimum Price per Unit in the dataset.
 --Expected output:Min_Price_per_unit
 
 SELECT MIN(price_per_unit)AS Min_Price_per_Unit
 FROM retail_sales_dataset;
+--------------------------------------------------------------------------------------------------
 
 --Find the number of transactions per Product Category.
 --Expected output: Product_Category, Transaction_Count
@@ -96,6 +110,7 @@ SELECT product_category,
     COUNT(*)AS Transaction_count
 FROM retail_sales_dataset
 GROUP BY product_category;
+-------------------------------------------------------------------------------------------------
 
 --Find the total revenue(Total Amount)per gender.
 --Expected output:Gender, Total_Revenue
@@ -104,6 +119,7 @@ SELECT SUM(total_Amount)AS Total_Revenue,
           gender
 FROM retail_sales_dataset
 GROUP BY gender;
+----------------------------------------------------------------------------------------------------
 
 --Find the average Price per Unit per product category.
 --Expected output: Product_Category,Average_Price
@@ -112,6 +128,7 @@ SELECT product_category,
     AVG(price_per_Unit)AS avg_price
 FROM retail_sales_dataset
 GROUP BY product_category;
+------------------------------------------------------------------------------------------------
 
 --Find the total per product category where total revenue is greater than 10,000.
 --EXpected output: Product_Category,Total_Revenue
@@ -121,6 +138,7 @@ SELECT product_category,
 FROM retail_sales_dataset
 GROUP BY product_category
 HAVING total_revenue>10000;
+---------------------------------------------------------------------------------------------------
 
 --Find the average quantity per product category where the average is more than 2.
 --Expected output:Product_category,Average_Quantity
@@ -130,6 +148,7 @@ SELECT product_category,
 FROM retail_sales_dataset
 GROUP BY product_category
 HAVING average_quantity >2
+---------------------------------------------------------------------------------------------------
 
 --Display a column called Spending_level that shows 'High' if Total amount>1000 otherwise'Low'?
 --Expected output: Transaction_ID,Total_Amount,Spending_level
@@ -140,6 +159,7 @@ SELECT Transaction_id,
   ELSE 'Low'
   END AS Spending_Level
 FROM retail_sales_dataset
+-------------------------------------------------------------------------------------------------
 
 --Display a new column called Age_Group that labels customers as:
   --'Youth' if Age<30
@@ -157,5 +177,5 @@ SELECT
     ELSE 'Unknown'
   END AS age_group
 FROM customers;
-
+-------------------------------------------------------------------------------------------------
  
